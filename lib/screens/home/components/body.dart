023c7897df9015949a59b93dfb2c3e93f8a065b2
RainBowT0506 <rainbowt0506@gmail.com>
2024-10-18 10:01:37 +0800

    Implement HomeScreen with a custom AppBar and search functionality
     Implement HomeScreen with a custom AppBar and search functionality
    
    - Created the HomeScreen widget as the main entry point of the plant app.
    - Added a customized AppBar with a title and a menu icon using SVG.
    - Developed the Body widget to include the Header component.
    - The Header features a greeting message and a logo.
    - Implemented a search bar within the Header for improved user navigation.

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/header.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[Header(size: size)],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plant_app/screens/home/components/body.dart';
+import 'package:plant_app/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text("Home",
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSecondary,
+              )),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
