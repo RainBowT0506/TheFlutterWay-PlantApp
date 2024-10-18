8d1df3a3dd374e0c39ff9a4f789e963a8f2153ab
RainBowT0506 <rainbowt0506@gmail.com>
2024-10-18 15:30:20 +0800

    feat: enhance plant detail screen
    - Added PlantDetailInfo widget to display plant title, country, and price
    - Implemented Buy Now and Description buttons in the Body widget

--- lib/screens/detail/components/body.dart
+++ lib/screens/detail/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/detail/components/icon_card.dart';
 import 'package:plant_app/screens/detail/components/plant_detail_header.dart';
+import 'package:plant_app/screens/detail/components/plant_detail_info.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -14,6 +15,42 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           PlantDetailHeader(),
+          PlantDetailInfo(
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+          ),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    backgroundColor: kPrimaryColor,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Text("Description"),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: kDefaultPadding * 2),
         ],
       ),
     );
